1a66e4e7d55e0c1f281b2d5878f7af0c0922f556
Reztsov Alexander <areztsov@neoflex.ru>
2021-12-08 16:14:45 +0300

    added docker-compose.yml, updated orders pom.xml, added Order.java

---
+++ orders-service/src/main/java/com/example/ordersservice/model/Order.java
@@ -0,0 +1,50 @@
+package com.example.ordersservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+@Entity
+@Table
+public class Order {
+
+    @Id
+    UUID id;
+
+    @NotNull
+    UUID customerId;
+
+    @NotNull
+    String orderStatus;
+
+    @NotNull
+    String deliveryAddress;
+
+    @NotNull
+    String cardDetails;
+
+    @NotNull
+    String dishesList;
+
+    @NotNull
+    String paymentStatus;
+
+    @NotNull
+    String restaurantStatus;
+
+    @NotNull
+    String deliveryStatus;
+}
