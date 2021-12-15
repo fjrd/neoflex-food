9e958faef085a9855f124eb31ff89a50e78120b3
Maxim Serov <mserov@neoflex.ru>
2021-12-15 17:28:40 +0300

    change  into FullOrderDto cardDetails

--- shared-order-dto/src/main/java/dto/CardDetailDto.java
+++ shared-order-dto/src/main/java/dto/CardDetailDto.java
@@ -1,8 +1,10 @@
 package dto;
 
-public record CardDetailDto(String cardNumber,
-                            String validDate,
-                            String firstName,
-                            String lastName,
-                            String cvc) {
+import javax.validation.constraints.NotBlank;
+
+public record CardDetailDto(@NotBlank String cardNumber,
+                            @NotBlank String validDate,
+                            @NotBlank String firstName,
+                            @NotBlank String lastName,
+                            @NotBlank String cvc) {
 }
