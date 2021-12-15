e3c35e683a5156685510ad5cca47b3e6511fbfbc
Maxim Serov <mserov@neoflex.ru>
2021-12-15 17:33:07 +0300

    change add builder

--- shared-order-dto/src/main/java/dto/CardDetailDto.java
+++ shared-order-dto/src/main/java/dto/CardDetailDto.java
@@ -1,7 +1,10 @@
 package dto;
 
+import lombok.Builder;
+
 import javax.validation.constraints.NotBlank;
 
+@Builder
 public record CardDetailDto(@NotBlank String cardNumber,
                             @NotBlank String validDate,
                             @NotBlank String firstName,
