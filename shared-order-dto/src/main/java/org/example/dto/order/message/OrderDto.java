b1ac64c78d057072c6bbd68c5a80875d5072a395
Reztsov Alexander <areztsov@neoflex.ru>
2021-12-16 09:19:52 +0300

    changed dto package structure

--- shared-order-dto/src/main/java/org/example/dto/order/message/OrderDto.java
+++ shared-order-dto/src/main/java/org/example/dto/order/message/OrderDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.example.dto.card.message.CardDetailDto;
+import org.example.dto.payment.message.CardDetailDto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
