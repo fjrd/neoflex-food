316bf758fb4b015c36695d2e55d5bc2914800e32
Maxim Serov <mserov@neoflex.ru>
2021-12-16 10:27:55 +0300

    add PaymentDto

--- shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentDto.java
+++ shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentDto.java
@@ -1,4 +1,11 @@
 package org.example.dto.payment.message;
 
-public record PaymentDto() {
+import javax.validation.constraints.NotBlank;
+
+public record PaymentDto(@NotBlank Integer cardNumber,
+                         @NotBlank Integer cvvCode,
+                         @NotBlank String cardExpireDate,
+                         @NotBlank String cardHolderName,
+                         @NotBlank PaymentStatus status) {
 }
+
--- shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentStatus.java
+++ shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentStatus.java
@@ -1,4 +1,10 @@
 package org.example.dto.payment.message;
 
 public enum PaymentStatus {
+    ACTIVE("active"),
+    INACTIVE("inactive");
+
+    PaymentStatus(String status) {
+
+    }
 }
