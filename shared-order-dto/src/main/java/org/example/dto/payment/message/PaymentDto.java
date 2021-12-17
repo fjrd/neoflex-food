7fabfa5baa062a9e02f9dddacb725abee724e483
Maxim Serov <mserov@neoflex.ru>
2021-12-17 16:33:59 +0300

    fix

--- shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentDto.java
+++ shared-order-dto/src/main/java/org/example/dto/payment/message/PaymentDto.java
@@ -1,11 +1,13 @@
 package org.example.dto.payment.message;
 
 import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
 
 public record PaymentDto(@NotBlank Integer cardNumber,
                          @NotBlank Integer cvvCode,
                          @NotBlank String cardExpireDate,
                          @NotBlank String cardHolderName,
-                         @NotBlank PaymentStatus status) {
+                         @NotBlank PaymentStatus status,
+                         @NotBlank BigDecimal orderAmount) {
 }
 
