ec3062e515bec1e73e76a80f916dc39d8af20867
Reztsov Alexander <areztsov@neoflex.ru>
2021-12-21 16:35:39 +0300

    fixed routing

--- gateway_api/src/main/java/com/neoflex/gateway_api/config/GatewayConfig.java
+++ gateway_api/src/main/java/com/neoflex/gateway_api/config/GatewayConfig.java
@@ -1,17 +1,18 @@
 package com.neoflex.gateway_api.config;
 
+import com.neoflex.gateway_api.config.properties.CustomApplicationProperties;
 import com.neoflex.gateway_api.filter.JwtAuthenticationFilter;
-import com.neoflex.gateway_api.filter.LoggingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class GatewayConfig {
+
+    @Autowired
+    private CustomApplicationProperties properties;
 
     @Autowired
     private JwtAuthenticationFilter authFilter;
@@ -21,10 +22,10 @@ public class GatewayConfig {
         return builder.routes()
                 .route("auth", r -> r.path("/auth/**")
                         .filters(f -> f.filter(authFilter))
-                        .uri("http://localhost:8092/"))
-                .route("auth2", r -> r.path("/api/v1/orders")
+                        .uri(properties.getCustomersHost()))
+                .route("auth2", r -> r.path("/api/v1/orders/**")
                         .filters(f -> f.filter(authFilter))
-                        .uri("http://localhost:8081/"))
+                        .uri(properties.getOrdersHost()))
                 .build();
 
     }
